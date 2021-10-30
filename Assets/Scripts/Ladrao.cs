15ba4a801dfe605715213edfe2401c6e4f779503
Álvaro Carvalho <a11v1r15@Rocketmail.com>
2021-10-30 11:59:03 -0300

    ✨ Basic movement

--- Assets/Scripts/Ladrao.cs
+++ Assets/Scripts/Ladrao.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Ladrao : MonoBehaviour
 {
+    private Rigidbody _rigidbody;
+    [SerializeField]
+    protected float _speed;
+    [SerializeField]
+    protected Vector3 _direction;
+
+
+    public Rigidbody GetRigidbody => _rigidbody ? _rigidbody : _rigidbody = gameObject.GetComponent<Rigidbody>();
+    public Vector3 GetDirection { get => _direction.normalized; set { _direction = value.normalized; } }
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
-    void Update()
+    virtual protected void Update()
     {
-        
+        GetRigidbody.MovePosition(GetRigidbody.position + transform.TransformDirection(GetDirection * Time.fixedDeltaTime * _speed));
+        transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.y);
     }
+
+
+#if UNITY_EDITOR
+    public void Validate() => OnValidate();
+    public virtual void OnValidate()
+    {
+        _direction = _direction.normalized;
+    }
+#endif
 }
